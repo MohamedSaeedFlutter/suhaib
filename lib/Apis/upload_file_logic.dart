aabcd026ddd11e43edc0caa20d4c06f4a72e33e1
Mohamed Saeed <muhammedsaidsyed215@gmail.com>
2021-11-08 12:51:49 +0200

    file uploaded

--- lib/Apis/upload_file_logic.dart
+++ lib/Apis/upload_file_logic.dart
@@ -1,6 +1,9 @@
 import 'dart:io';
+import 'dart:math';
 import 'package:bloc/bloc.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:file_picker/file_picker.dart';
+import 'package:firebase_database/firebase_database.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:suhaib/models/files_model.dart';
@@ -23,13 +26,9 @@ class UploadFileLogic extends Cubit<ChangeChatState>  {
       print(fileExtension);
       var uuid = const Uuid().v4();
       final Reference firebaseStorageRef = FirebaseStorage.instance
-          .ref()
-          .child('sahib/data/${amId}$uuid/');
+          .ref().child('sahib/data/${amId}$uuid/');
       await firebaseStorageRef.putFile(localFile)
-          .catchError((onError) {
-        print(onError);
-        return false;
-      });
+          .catchError((onError) {print(onError); return false;});
       String url = await firebaseStorageRef.getDownloadURL();
       print("download url: $url");
       Files file = Files(
@@ -76,6 +75,50 @@ class UploadFileLogic extends Cubit<ChangeChatState>  {
     print(person.id);
     return person;
   }
+
+
+  //
+  // final mainReference = FirebaseDatabase.instance.reference().child('Database');
+  // Future getPdfAndUpload()async{
+  //   var rng = new Random();
+  //   String randomName="";
+  //   for (var i = 0; i < 20; i++) {
+  //     print(rng.nextInt(100));
+  //     randomName += rng.nextInt(100).toString();
+  //   }
+  //   FilePickerResult result = await FilePicker.platform.pickFiles();
+  //   File file = File(result.files.first.path);
+  //       // .getFile(type: FileType.custom, fileExtension: 'pdf')
+  //   String fileName = '${randomName}.pdf';
+  //   print(fileName);
+  //   print('${file.readAsBytesSync()}');
+  //   savePdf(file.readAsBytesSync(), fileName);
+  // }
+  //
+  // Future savePdf(List<int> asset, String name) async {
+  //
+  //   Reference reference = FirebaseStorage.instance.ref().child(name);
+  //   UploadTask uploadTask = reference.putData(asset);
+  //   String url = await (await uploadTask..onComplete).ref.getDownloadURL();
+  //   print(url);
+  //   documentFileUpload(url);
+  //   return  url;
+  // }
+  // void documentFileUpload(String str) {
+  //
+  //   var data = {
+  //     "PDF": str,
+  //   };
+  //   mainReference.child("Documents").child('pdf').set(data).then((v) {
+  //   });
+  // }
+
+
+
+
+
+
+
 
 
 
--- lib/ui/upload_file.dart
+++ lib/ui/upload_file.dart
@@ -1,5 +1,6 @@
 import 'dart:io';
 
+import 'package:file_picker_cross/file_picker_cross.dart';
 import 'package:file_picker/file_picker.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
@@ -16,43 +17,49 @@ final String category;
 }
 
 class _UploadFileState extends State<UploadFile> {
+
   var auth = FirebaseAuth.instance.currentUser;
-  File fileOutput;
   TextEditingController pdfController ;
-_sendFile(){
-  UploadFileLogic.get(context).sendFile(
-      amId: auth.uid, localFile: AuthApi.get(context).fileOutput,
-      cat: widget.category , sub: pdfController.text
-  ).then((value) => Fluttertoast.showToast(
-      msg: "The File uploaded successfully !"));
-}
+  File fileOutput;
+  FilePickerCross filePickerCross;
+  String _fileString = '';
+  Set<String> lastFiles;
+  FileQuotaCross quota = FileQuotaCross(quota: 0, usage: 0);
+
   _pickFile() async {
     FilePickerResult result = await FilePicker.platform.pickFiles();
     if (result != null) {
-       AuthApi.get(context).setFileOutput(
-         file: File(result.files.single.path)
-       );
+      File file = File(result.files.single.path);
     } else {
-        print("File picker is cancelled");}
+print("User canceled the picker");    }
   }
+
+  _sendFile(){
+    UploadFileLogic.get(context).sendFile(
+        amId: auth.uid, localFile: fileOutput,
+        cat: widget.category , sub: pdfController.text
+    ).then((value) => Fluttertoast.showToast(
+        msg: "The File uploaded successfully !"));
+  }
+
 
   @override
   Widget build(BuildContext context) {
+
     pdfController =
-    TextEditingController(text:
-    fileOutput?.path?.split('/')?.last.toString()??"");
-print("1////${fileOutput?.path?.split('/')?.last.toString()??""}");
+    TextEditingController(text:filePickerCross?.path ?? 'unknown' );
+        print("1////${filePickerCross?.path ?? 'unknown'}");
     return Scaffold(
       body: Center(child:
     Column(
       mainAxisAlignment: MainAxisAlignment.center,
       crossAxisAlignment: CrossAxisAlignment.center,
       children: [
-      InkWell(
-        onTap: _pickFile,
+      GestureDetector(
         child: Image.asset("assets/intro/pdf6.png",
         width: MediaQuery.of(context).size.width * 0.5,
         height: MediaQuery.of(context).size.height * 0.25,),
+        onTap:() => _selectFile(context),
       ),
         Padding(
           padding: const EdgeInsets.only(top: 18),
@@ -69,7 +76,15 @@ print("1////${fileOutput?.path?.split('/')?.last.toString()??""}");
           child: SizedBox(
             width: MediaQuery.of(context).size.width * 0.95,
             child: ElevatedButton(
-              onPressed: _sendFile ,
+              onPressed: () {
+print("1////${AuthApi.get(context).fileOutput?.path?.split('/')?.last?.toString()??""}");
+                UploadFileLogic.get(context).sendFile(
+                    amId: auth.uid,
+                    localFile: File(filePickerCross?.path),
+                    cat: widget.category , sub: pdfController.text
+                ).then((value) => Fluttertoast.showToast(
+                    msg: "The File uploaded successfully !"));
+              },
               style: ElevatedButton.styleFrom(
                 primary: Color.fromRGBO(155, 156, 184 , 1),
                 // onPrimary: Colors.white,
@@ -86,4 +101,36 @@ print("1////${fileOutput?.path?.split('/')?.last.toString()??""}");
         ),
       ],),),);
   }
+  void _selectFile(context) {
+    FilePickerCross.importMultipleFromStorage().then((filePicker) {
+      setFilePicker(filePicker[0]);
+      print("filePicker555555555${filePicker.single.path}");
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text('You selected ${filePicker.length} file(s).'),
+        ),
+      );
+
+      setState(() {});
+    });
+  }
+
+
+
+  setFilePicker(FilePickerCross filePicker) => setState(() {
+    filePickerCross = filePicker;
+    filePickerCross.saveToPath(path: filePickerCross.fileName);
+    FilePickerCross.quota().then((value) {
+      setState(() => quota = value);
+    });
+    lastFiles.add(filePickerCross.fileName);
+    try {
+      _fileString = filePickerCross.toString();
+    } catch (e) {
+      _fileString = 'Not a text file. Showing base64.\n\n' +
+          filePickerCross.toBase64();
+    }
+  });
+
+
 }
